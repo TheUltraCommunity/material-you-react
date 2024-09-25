c979961d30aaf1fb65648bd6489ead94f236469a
Chandan-6 <khamitkarsaichandan1035@gmail.com>
2024-09-25 19:43:05 +0530

    Added Sudo classes to BottomAppBar
    Signed-off-by: Chandan-6 <khamitkarsaichandan1035@gmail.com>
    
    * Hover
    * Active

--- src/components/AppBars/BottomAppBar/BottomAppBar.tsx
+++ src/components/AppBars/BottomAppBar/BottomAppBar.tsx
@@ -7,23 +7,17 @@ type BottomAppBarProps = {
 };
 
 export default function BottomAppBar(props: BottomAppBarProps) {
-<<<<<<< HEAD
-    const [isHovered, setIsHovered] = useState<boolean>(false);
-    const [isFocused, setIsFocused] = useState<boolean>(true);
-    const [isActive, setIsActive] = useState<boolean>(true);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
-    return (
-        <div
-=======
   return (
     <div
       style={{
-        backgroundColor: `${
-          props.backgroundColor ?? "rgb(var(--md-sys-color-surface-container))"
-        }`,
+        backgroundColor: `${props.backgroundColor ?? "rgb(var(--md-sys-color-surface-container))"
+          }`,
         width: "100vw",
         height: "80px",
-        padding: "12px 16px 12px 16px",
+        padding: `${props.floatingActionButton ? '16px' : '12px 16px'}`,
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
@@ -42,13 +36,19 @@ export default function BottomAppBar(props: BottomAppBarProps) {
       >
         {props.items.map((element, index) => (
           <span
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            onMouseDown={() => setIsActive(true)}
+            onMouseUp={() => setIsActive(false)}
             key={index}
-            className="material-symbols-rounded"
->>>>>>> 07ef7631be9803d6a5fac61a7afdabf63ba0afca
+            className={`
+              ${!isHovered ? "md-elevation-0" : "md-elevation-1"}
+              label-large material-symbols-rounded`}
             style={{
               width: "24px",
               height: "24px",
-              color: "rgb(var(--md-sys-color-on-secondary-container))",
+              cursor: 'pointer',
+              color: `${isActive ? 'rgb(var(--md-sys-color-on-tertiary-container))' : 'rgb(var(--md-sys-color-on-secondary-container))'}`,
             }}
           >
             {element || "add"}
@@ -56,69 +56,7 @@ export default function BottomAppBar(props: BottomAppBarProps) {
         ))}
       </div>
 
-<<<<<<< HEAD
-
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    gap: '16px',
-                }}
-            >
-                {
-                    props.items.map((element, index) => <span
-                        key={index}
-                        className="material-symbols-outlined"
-                        style={
-                            {
-                                width: '24px',
-                                height: '24px',
-                                color: 'rbg(var(--md-sys-color-on-secondary-container))'
-                            }}>{element || 'add'}</span>)
-                }
-            </div>
-
-            {
-                props.floatingActionButton && (
-                    <div
-                        className="md-elevation-2"
-                        style={{
-                            backgroundColor: `${props.floatingActionButtonBackgroundColor ?? 'rbg(var(--md-sys-color-primary-container))'}`,
-                            width: '56px',
-                            height: '56px',
-                            borderRadius: '16px',
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <span
-                            onMouseEnter={() => setIsHovered(true)}
-                            onMouseLeave={() => setIsHovered(false)}
-                            onFocus={() => setIsFocused(true)}
-                            onBlur={() => setIsFocused(false)}
-                            onMouseDown={() => setIsActive(true)}
-                            onMouseUp={() => setIsActive(false)}
-                            className="material-symbols-outlined"
-                            style={{
-                                width: '24px',
-                                height: '24px',
-                                color: 'rbg(var(--md-sys-color-on-primary-container))'
-                            }}
-                        >
-                            {props.floatingActionButtonIcon || 'add'}
-                        </span>
-                    </div>
-                )
-
-            }
-        </div>
-    );
-};
-=======
       {props.floatingActionButton}
     </div>
   );
 }
->>>>>>> 07ef7631be9803d6a5fac61a7afdabf63ba0afca
