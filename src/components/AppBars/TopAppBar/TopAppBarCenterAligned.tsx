7aaa82b18d766b192fb3fa3b1a7861d071aa5c7a
Manas Malla <manasmalla.dev@gmail.com>
2024-09-26 19:44:35 +0530

    Fixed `TopAppBarSmall` and `TopAppBarCenterAligned`
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/components/AppBars/TopAppBar/TopAppBarCenterAligned.tsx
+++ src/components/AppBars/TopAppBar/TopAppBarCenterAligned.tsx
@@ -1,77 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 
 type TopAppBarCenterAlignedProps = {
-    containerBackgroundColor?: string;
-    headline: string;
-    leadingIcon?: string;
-    trailingIcon?: string;
-}
-
-export default function TopAppBarCenterAligned(props: TopAppBarCenterAlignedProps) {
-    return (
-        <div
-            style={{
-                backgroundColor: `${props.containerBackgroundColor ?? "rgb(var(--md-sys-color-surface-container))"}`,
-                width: '100vw',
-                height: '64px',
-                borderRadius: '0px',
-                padding: '0px 16px',
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                gap: '24px',
-                position: 'fixed',
-                top: '0',
-                left: '0'
-            }}
-        >
-
-            {/* LeadingIcon */}
-            <div
-                style={{
-                    width: '24px',
-                    height: '24px',
-                    color: 'rgb(var(--md-sys-color-surface))',
-                    cursor: 'pointer'
-                }}
-            >
-                <span
-                    className="material-symbols-rounded"
-                >
-                    {props.leadingIcon || 'menu'}
-                </span>
-            </div>
-
-            {/* Headline */}
-            <p
-                style={{
-                    fontWeight: 'var(--md-sys-typescale-title-large-weight)',
-                    fontSize: 'var(--md-sys-typescale-title-large-size)',
-                    lineHeight: 'var(--md-sys-typescale-title-large-line-height)',
-                    letterSpacing: 'var(--md-sys-typescale-title-large-tracking)',
-                    backgroundColor: 'rgb(var(--md-sys-color-on-surface: 29, 27, 32))'
-
-                }}
-            >
-                {props.headline}
-            </p>
-
-            {/* Trailing Icon */}
-            <div
-                style={{
-                    width: '24px',
-                    height: '24px',
-                    color: 'rgb(var(--md-sys-color-on-surface-variant))',
-                    cursor: 'pointer'
-                }}
-            >
-                <span
-                    className="material-symbols-rounded"
-                >
-                    {props.trailingIcon || 'account_circle'}
-                </span>
-            </div>
-
-        </div>
-    );
+  containerBackgroundColor?: string;
+  headline: string;
+  leadingIcon?: string;
+  avatar?: boolean;
+  avatarImage?: string;
 };
+
+export default function TopAppBarCenterAligned(
+  props: TopAppBarCenterAlignedProps
+) {
+  const [isScrolled, setScrollState] = useState(false);
+  window.onscroll = function () {
+    if (window.scrollY > 0) {
+      setScrollState(true);
+    } else {
+      setScrollState(false);
+    }
+  };
+  return (
+    <div
+      className={isScrolled ? "md-elevation-2" : "md-elevation-0"}
+      style={{
+        backgroundColor: `${
+          props.containerBackgroundColor ??
+          (isScrolled
+            ? "rgb(var(--md-sys-color-surface-container))"
+            : "rgb(var(--md-sys-color-surface))")
+        }`,
+        position: "sticky",
+        width: "100vw",
+        height: "64px",
+        borderRadius: "0px",
+        padding: "0px 16px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        gap: "24px",
+        top: "0",
+        left: "0",
+        color: "rgb(var(--md-sys-color-on-surface))",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "48px",
+          height: "48px",
+          cursor: "pointer",
+        }}
+      >
+        {/* LeadingIcon */}
+        {props.leadingIcon && (
+          <div
+            style={{
+              width: "24px",
+              height: "24px",
+            }}
+          >
+            <span className="material-symbols-rounded">
+              {props.leadingIcon || "arrow_back"}
+            </span>
+          </div>
+        )}
+      </div>
+      {/* Headline */}
+      <p
+        className="title-large"
+        style={{
+          width: "fit-content",
+          color: "rgb(var(--md-sys-color-on-surface))",
+        }}
+      >
+        {props.headline}
+      </p>
+      {/* Trailing Icons */}
+      {(props.avatar || props.avatarImage) && (
+        <div
+          key={"avatar"}
+          style={{
+            width: "48px",
+            fontSize: "30px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "48px",
+            cursor: "pointer",
+            color: "rgb(var(--md-sys-color-on-surface-variant))",
+          }}
+        >
+          {props.avatarImage ? (
+            <img
+              src={props.avatarImage}
+              style={{
+                width: "30px",
+                height: "30px",
+                objectFit: "cover",
+                borderRadius: "15px",
+              }}
+            />
+          ) : (
+            <span
+              className="material-symbols-rounded"
+              style={{
+                fontSize: "30px",
+              }}
+            >
+              account_circle
+            </span>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
