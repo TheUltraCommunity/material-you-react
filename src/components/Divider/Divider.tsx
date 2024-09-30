612ffd676d1062fbc799ce5e37ce455d57fc507c
Chandan-6 <khamitkarsaichandan1035@gmail.com>
2024-09-30 18:04:36 +0530

    Changed types in Divider Component
    Signed-off-by: Chandan-6 <khamitkarsaichandan1035@gmail.com>
    
    * If type = horizontal, then variant can be accepted as props.
    * If type = vertical, then variant is not required.

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-type DividerProps = {
-    type: "horizontal" | "vertical";
-    variant : "fullWidth" | "inset" | "middleInset" | "rightMargin";
-}
+type DividerProps = 
+    | { type : 'horizontal'; variant : "fullWidth" | "inset" | "middleInset" | "rightMargin"}
+    | { type : 'vertical'; variant?: never};
+
 
 /**
  * @description A Divider Component - Used to differentiate items.
- * @params variant: string ( *Allowed Inputs* = `fullWidth`, `inset`, `middleInset`, `rightMargin` , `bottomMargin`)
+ * @params type : string ( `horizontal` | `vertical` )
+ * @params variant: string ( *Allowed Inputs ( 🔴 NOTE: only if type : horizontal )* = `fullWidth` | `inset` | `middleInset` | `rightMargin` )
 */
 
 export default function Divider( props: DividerProps ) {
@@ -22,7 +23,8 @@ export default function Divider( props: DividerProps ) {
                                 "8px" : "32px" })`
                                      : "1px" }`,
                 height: `${props.type === 'horizontal' ? "1px" : "100%" }`,
-                marginLeft: props.variant === 'inset' || props.variant === 'middleInset' ? "16px" : '',
+                marginLeft: props.type === 'horizontal' ? 
+                    props.variant === 'inset' || props.variant === 'middleInset' ? "16px" : '' : '',
                 backgroundColor: 'rgb(var(--md-sys-color-outline-variant))',
                 marginBottom : props.type === 'horizontal' ? "8px" : ''
             }}
