718d56a65c8c8c1c7c3ea52b73aa948eccdf1397
Chandan-6 <khamitkarsaichandan1035@gmail.com>
2024-10-21 10:10:25 +0530

    Fixed the scrolling effect issue
    Signed-off-by: Chandan-6 <khamitkarsaichandan1035@gmail.com>

--- src/components/Tabs/TabContext.ts
+++ src/components/Tabs/TabContext.ts
@@ -3,11 +3,13 @@ import React, { useContext, createContext } from "react";
 interface TabContextType {
   activeTabId: string | null;
   setActiveTabId: (id: string) => void;
+  requiredWidth : string
 }
 
 export const TabContext = createContext<TabContextType>({
   activeTabId: null,
   setActiveTabId: () => {},
+  requiredWidth: '90px'
 });
 
 export const useTabContext = () => useContext(TabContext);
