fa0a6b208f5feefca31be26485ef67b2b88f6f59
ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>
2025-02-20 14:39:31 +0530

    slider alpha v0.2
    Signed-off-by: ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>

--- src/components/Sliders/Slider.tsx
+++ src/components/Sliders/Slider.tsx
@@ -1,7 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 export default function Slider() {
-    const [value, setValue] = useState<number>(40);
+    const [value, setValue] = useState<number>(40); // Value Percentage
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [isDragging, setIsDragging] = useState<boolean>(false);
+
+
+    useEffect(() => {
+        console.log("value precentage : ", value);
+    }, [value]);
+
+
+    const handleMouseDown = (e: any) => {
+        setIsDragging(true);
+    }
+
+    const handleMouseUp = (e: any) => {
+        setIsDragging(false);
+    }
+
+    const handleMouseMove = (e: any) => {
+        if (isDragging) {
+            const container = containerRef.current;
+            if (container) {
+                const containerRect = container.getBoundingClientRect();
+                const x = e.clientX - containerRect.left;
+                const pxInPercentage = (x * 100) / containerRect.width;
+                const clampedValue = Math.round(Math.max(0, Math.min(100, pxInPercentage)));
+                setValue(clampedValue);
+            }
+        }
+    };
 
     return (
         // Parent Container
@@ -12,7 +41,11 @@ export default function Slider() {
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
+                cursor: isDragging ? "ew-resize" : "default",
             }}
+            ref={containerRef}
+            onMouseUp={handleMouseUp}
+            onMouseMove={handleMouseMove}
         >
 
             {/* Active Track 🟩 */}
@@ -21,9 +54,12 @@ export default function Slider() {
                     width: `${value}%`,
                     height: "16px",
                     background: "#65558F",
-                    borderTopLeftRadius: "48px",
-                    borderBottomLeftRadius: "48px",
-                    position: "relative"
+                    borderTopLeftRadius: "16px",
+                    borderTopRightRadius: "2px",
+                    borderBottomLeftRadius: "16px",
+                    borderBottomRightRadius: "2px",
+                    position: "relative",
+                    marginRight: "6px"
                 }}
             >
                 {/* Start Indicator */}
@@ -36,7 +72,8 @@ export default function Slider() {
                         borderRadius: "100%",
                         left: 6,
                         top: "50%",
-                        transform: "translateY(-50%)"
+                        transform: "translateY(-50%)",
+                        opacity: value == 0 ? 0 : 1
                     }}
                 >
                 </div>
@@ -47,12 +84,25 @@ export default function Slider() {
                 style={{
                     width: "4px",
                     height: "44px",
-                    background: "#65558F",
-                    borderRadius: "20px",
+                    background: "transparent",
+                    borderRadius: "2px",
                     cursor: "ew-resize",
-                    margin: "0 6px"
+                    position: "relative"    
                 }}
+                onMouseDown={handleMouseDown}
             >
+                <div
+                    style={{
+                        width: "4px",
+                        height: "44px",
+                        background: "#65558F",
+                        borderRadius: "20px",
+                        position: "absolute",
+                        left: "50%",
+                        transform: "translateX(-50%)"
+                    }}
+                >
+                </div>
             </div>
 
             {/* Inactive Track 🔴 */}
@@ -61,9 +111,12 @@ export default function Slider() {
                     flex: 1,
                     height: "16px",
                     background: "rgb(var(--md-sys-color-on-surface-variant))",
-                    borderTopRightRadius: "48px",
-                    borderBottomRightRadius: "48px",
-                    position: "relative"
+                    borderTopRightRadius: "16px",
+                    borderTopLeftRadius: "2px",
+                    borderBottomRightRadius: "16px",
+                    borderBottomLeftRadius: "2px",
+                    position: "relative",
+                    marginLeft: "6px"
                 }}
             >
                 {/* End Indicator */}
@@ -76,7 +129,8 @@ export default function Slider() {
                         borderRadius: "100%",
                         right: 6,
                         top: "50%",
-                        transform: "translateY(-50%)"
+                        transform: "translateY(-50%)",
+                        opacity: value == 100 ? 0 : 1
                     }}
                 >
                 </div>
